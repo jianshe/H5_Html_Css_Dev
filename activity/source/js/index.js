32340637a279aff41f13db49b0b7ef5b3d8582b8
zhangjianshe <zhangjianshe@eastmoney.com>
2018-04-12 16:40:35 +0800

    update files

--- activity/source/js/index.js
+++ activity/source/js/index.js
@@ -25,14 +25,16 @@
     };
     var scale = deviceData.w / deviceData.h;
 
-    // document.addEventListener("WeixinJSBridgeReady", function() {
-    //     WeixinJSBridge.call('hideOptionMenu');
-    //     //toggleMusic();
-    //     audio.play();
-    //     paiDui.play();
-    //     paiDui.pause();
-    //     wxFirstLoad = false;
-    // }, false);
+    document.addEventListener("WeixinJSBridgeReady", function() {
+        WeixinJSBridge.call('hideOptionMenu');
+        //toggleMusic();
+        audio.play();
+        paiDui.play();
+        paiDui.pause();
+        jianPan.play();
+        jianPan.pause();
+        wxFirstLoad = false;
+    }, false);
 
     var preImg = [
         '../resource/img/niu1.png',
@@ -182,31 +184,114 @@
         '../resource/img/gn-title.png'
     ];
     $(function() {
-        var $stage = $('#stage');
-        var $loading = $('#loading');
-        var $percent = $('#percent');
-        var $music = $('#music');
+        var $stage = $('#stage'); //动画显示父级类
+        var $loading = $('#loading'); //loading父级类
+        var $percent = $('#percent'); //初始化页面加载百分比
+        var $music = $('#music'); //右上角音乐图标
         var $nextBtn = $('[data-next]'); //翻开相册
-        var $slideUp = $('#slideUp'); //
+
+        var $syjPlay = $('#syjPlay'); //播放按钮
+        var $lineDjbf = $('#lineDjbf'); //点击播放
+
+        var $slideUp = $('#slideUp'); //swiper滑动方向指示
         var $tg = $('#tg'); //点击跳过
+
+
+        var $p8Texts = $('#p8-text').find('.item'); //手机页面切换元素
+
         var $np2PopStock = $('#np2PopStock'); //股票点击放大元素
+        var $sayStory = $('#sayStory'); //说说你的股市
 
-        var _Swiper = null;
-        var startTimer = null;
+        var $wyly = $('#wyly'); //获奖名单
+        var $lyCloseBtn = $('#lyCloseBtn'); //弹出的获奖名单页面，关闭操作
+
+        var $lyListBack = $('#lyListBack'); //返回上页
+
+        var $goTop = $('#goTop'); //鼠标滚动时，右侧悬浮窗始终位于页面中间
+        var $scrollWrap = $('[data-list-scroll]'); //股友回忆录父容器
 
 
+        var $lyCount = $('#lyCount'); //获取用户输入内容总字数
+        var $lyTextarea = $('#lyTextarea'); //指向文本输入框
+        var $submitBtn = $('#submitBtn'); //确认提交股事
+
+
+        //感恩回馈
+        var $toGnShare = $('#toGnShare');
+        var $gnBackBtn = $('#gnBackBtn');
+        var $gnShareBtn = $('#gnShareBtn');
+        var $shareMask = $('#shareMask');
+
+
+        var _lbSprite = null; //创建精灵图标类的实例
+        var _Swiper = null; //整个页面切换swiper类
+        var _SwiperP7 = null; //手机屏幕功能页面切换
+        var _p7Timer = null; //设置一个定时器，阻止_SwiperP7自动滚动
+        var startTimer = null; //设置一个定时器，延时切换到第一页
+        var pdTimer = null; //设置一个器，延时播放排队背景音乐
+	var jpTimer = null;//设置一个定时器，延时播放敲键盘背景音乐播放
         var startTime = new Date().getTime();
 
+        $scrollWrap.on('scroll', function(e) {
+            $goTop[0].style.top = (this.scrollTop + 400) + 'px';
+        });
+        $goTop.on('click', function(e) {
+            $scrollWrap[0].scrollTop = 0;
+        });
+
+        shouYin.addEventListener('ended', stopShouYin, false);
+
         $music.on('click', toggleMusic);
+        $tg.on('click', function() {
+            _Swiper.unlockSwipes();
+            _Swiper.slideTo(8, 0, true);
+            clearTimeout(startTimer);
+        });
         $nextBtn.on('click', function() {
             $('.page1').find('.white-mask').show().addClass('mask-on');
             setTimeout(function() {
                 _Swiper.slideTo(2, 0, true);
             }, 800);
         });
-
         $np2PopStock.on('click', function() {
             $(this).toggleClass('showBig');
+        });
+        $syjPlay.on('click', function() {
+            var $this = $(this);
+            if ($this.attr('data-init') != 1) {
+                _lbSprite = new Sprite(document.getElementById('lb'), [
+                    '../resource/img/gbyx1.png',
+                    '../resource/img/gbyx2.png'
+                ], 1200);
+                $this.attr('data-init', 1);
+                $lineDjbf.hide();
+                _lbSprite.play();
+                audio.volume = 0.1;
+                btnPlay.play();
+                setTimeout(function() {
+                    shouYin.play();
+                }, 1000);
+            } else {
+                if (_lbSprite.isPlay) {
+                    _lbSprite.stop(true);
+                    $lineDjbf.show();
+                    btnPlay.pause && btnPlay.pause();
+                    shouYin.pause && shouYin.pause();
+                    audio.volume = volume;
+                } else {
+                    audio.volume = 0.1;
+                    _lbSprite.play();
+                    btnPlay.play();
+                    $lineDjbf.hide();
+                    setTimeout(function() {
+                        shouYin.play();
+                    }, 1000);
+                }
+            }
+            $this.toggleClass('bf-btn-on');
+        });
+        $sayStory.on('click', function() {
+            _Swiper.slideTo(10, 0, true);
         });
 
 
@@ -231,6 +316,48 @@
             } else {
                 $music.show().addClass('music-on');
             }
+        }
+        $toGnShare.on('click', function() {
+            _Swiper.slideTo(11, 0, true);
+        });
+
+        $gnBackBtn.on('click', turnBack);
+        $lyListBack.on('click', turnBack);
+        $lyCloseBtn.on('click', turnBack);
+
+        $wyly.on('click', function() {
+            _Swiper.unlockSwipes();
+            _Swiper.slideTo(10, 0, true);
+        });
+
+        $gnShareBtn.on('click', function() {
+            $shareMask.show();
+        });
+        $shareMask.on('click', function() {
+            $shareMask.hide();
+        });
+
+        $lyTextarea.on('keydown', function(e) {
+            var content = $.trim(this.value);
+            if (content.length > 1000) {
+                e.preventDefault();
+                e.stopPropagation();
+            } else {
+                $lyCount.text(content.length);
+            }
+        });
+        $('.ly-content').on('click', '.ly-page-input-1', function() {
+            $(this).removeClass('ly-page-input-1').prev().val('');
+        }).find('input').on('keyup', function() {
+            $(this).next()[!!$.trim(this.value) ? 'addClass' : 'removeClass']('ly-page-input-1');
+        });
+        $submitBtn.on('click', function() {
+            alert('提交留言')
+        });
+
+        function turnBack() {
+            _Swiper.unlockSwipes();
+            _Swiper.slideTo(8, 0, true);
         }
 
         function enter() {
@@ -258,19 +385,52 @@
                             $('.page2').find('.white-mask').show().removeClass('mask-on');
                             _Swiper.unlockSwipes();
                         }
+                        if (index !== 2) {
+                            //如果不是第二页，停止排除背景音乐播放，同时将股票显示为小图标
+                            $np2PopStock.removeClass('showBig');
+                            paiDui && paiDui.pause();
+                            clearTimeout(pdTimer);
+                        }
+                        //如果不是第三页，停止收音机背景音乐播放，同时还原初始化进入页面的设置
+                        if (index !== 3 && _lbSprite && _lbSprite.isPlay) {
+                            stopShouYin();
+                            $lineDjbf.show();
+                        }
                         pageSprite(swiper.activeIndex, swiper);
                         //var slideUpShowDelays = [0, 5, 5.5, 6.5, 4.5, 5.5, 4.5, 4.3, 0];
                         var slideUpShowDelays = [0, 0, 0, 0, 0, 0, 0, 0, 0];
                         if (index > 1 && index < 9) {
-                            console.log(slideUpShowDelays[index], '秒后才能滑动');
+                            // console.log(slideUpShowDelays[index], '秒后才能滑动');
                             setTimeout(function() {
                                 $slideUp.show();
                                 _Swiper.unlockSwipes();
                             }, slideUpShowDelays[index] * 1000);
                         }
+                        if (index !== 5) {
+                            jianPan && jianPan.pause();
+                            clearTimeout(jpTimer);
+                        }
+                        if (index > 7) {
+                            $tg.hide();
+                        } else {
+                            $tg.show();
+                        }
+                        if (index === 10) {
+                            $music.hide();
+                        } else {
+                            $music.show();
+                        }
                     }
                 });
                 window._Swiper = _Swiper;
+                _SwiperP7 = new Swiper('#p8-swiper', {
+                    pagination: '.p8-swiper-nav',
+                    autoplay: 1500,
+                    autoplayDisableOnInteraction: false,
+                    onSlideChangeStart: function(swiper) {
+                        $p8Texts.removeClass('text-active').eq(swiper.activeIndex).addClass('text-active');
+                    }
+                });
 
                 startTimer = setTimeout(function() {
                     _Swiper.slideTo(1, 0, true);
@@ -296,9 +456,20 @@
             }
         });
 
+        function stopShouYin() {
+            _lbSprite.stop(true);
+            $syjPlay.removeClass('bf-btn-on');
+            btnPlay.pause && btnPlay.pause();
+            shouYin.pause && shouYin.pause();
+            audio.volume = volume;
+            $lineDjbf.show();
+        }
         //页面动画精灵
         function pageSprite(index, swiper) {
             $('.stage').find('[data-sprite]').removeClass('sprite-on');
+            _SwiperP7 && _SwiperP7.stopAutoplay();
+            _SwiperP7.slideTo(0, 20, true);
+            clearTimeout(_p7Timer);
             var pageSpriteMap = {
                 'page0': function() {},
                 'page1': function() {},
@@ -323,7 +494,11 @@
                 'page6': function() {
                     initPageSprite(index);
                 },
-                'page7': function() {},
+                'page7': function() {
+                    _p7Timer = setTimeout(function() {
+                        _SwiperP7 && _SwiperP7.startAutoplay();
+                    }, 2000);
+                },
                 'page8': function() {
                     initPageSprite(index);
                 }
@@ -349,6 +524,9 @@
         Util.imageLoad({
             url: preImg,
             complete: function() {
+                //var loading = new Sprite(document.getElementById('loadingSprite'), [preImg[0], preImg[1]], 800);
+                //loading.play();
+                //适配
                 $('[data-scale]').show();
                 Util.scaleElem('[data-scale]', function(el) {
                     $(el).addClass('fadeIn');
@@ -361,4 +539,37 @@
             }
         });
     }
+
+    //精灵图类
+    function Sprite(el, src, duration) {
+        this.el = el;
+        this.src = src;
+        this.duration = duration;
+        this.frameIndex = 0;
+        this.timer = null;
+        this.isPlay = false;
+        this.render();
+    }
+
+    Sprite.prototype.render = function() {
+        this.el.style.background = 'url("' + this.src[this.frameIndex] + '") no-repeat center';
+        this.frameIndex = this.frameIndex >= this.src.length - 1 ? 0 : this.frameIndex + 1;
+    };
+    Sprite.prototype.play = function() {
+        var that = this;
+        this.isPlay = true;
+        this.timer = setInterval(function() {
+            that.render();
+        }, this.duration / this.src.length);
+    };
+    Sprite.prototype.stop = function(isDestroy) {
+        this.isPlay = false;
+        if (this.timer) {
+            clearInterval(this.timer);
+        }
+        if (isDestroy) {
+            this.el.style.background = 'none';
+            this.frameIndex = 0;
+        }
+    };
 }());
